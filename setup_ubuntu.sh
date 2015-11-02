28a3fdd3f0ebf289054fadbb5ad62cda034ba007
ZombineDev <petar.p.kirov@gmail.com>
2015-11-02 20:09:30 +0200

    Add installation of Package Control for Sublime-Text 3

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -16,6 +16,11 @@ sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
 
+# Add Package Control to Sublime-Text 3
+# 1) Start Sublime-Text 3 and do ctrl+`
+# 2) Enter:
+	import urllib.request,os,hashlib; h = '2915d1851351e5ee549c20394736b442' + '8bc59f460fa1548d1514676163dafc88'; pf = 'Package Control.sublime-package'; ipp = sublime.installed_packages_path(); urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) ); by = urllib.request.urlopen( 'http://packagecontrol.io/' + pf.replace(' ', '%20')).read(); dh = hashlib.sha256(by).hexdigest(); print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by)
+
 # Install VIM
 sudo apt-get install vim ctags vim-doc vim-scripts
 
