017102527f834c6eb9c773efb92cf0afaf63177b
ZombineDev <petar.p.kirov@gmail.com>
2018-11-01 01:40:38 +0200

    Add `docker-pull-run` fish command

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -65,5 +65,13 @@ function grmbr --argument-names remote master_branch feature_branch
     and git branch -d $feature_branch
 end
 
+
+# ----- Docker: -----
+function docker-pull-run --argument-names container_name image_name
+    docker kill $container_name; or true
+    docker pull $image_name
+    and docker run -p 3000:80 --rm -d --name $container_name $image_name
+end
+
 # Google Chrome aliases:
 alias igchr='google-chrome --incognito & disown'
