371f6758b4c6180839b2637159f2603c3ece222f
ZombineDev <petar.p.kirov@gmail.com>
2019-02-14 13:32:28 +0200

    [config.fish]: Add bobthefish theme settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,3 +11,12 @@ end
 # Set-up the PATH:
 set -p PATH (yarn global bin)
 set -p PATH "$HOME/bin"
+
+# bobthefish theme settings:
+set -g theme_newline_cursor yes
+set -g theme_date_format "+%H:%M:%S %F (%a)"
+set -g theme_color_scheme dark
+set -g theme_display_vi yes
+# If using fonts from this repo:
+# https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/FiraCode/Regular/complete
+set -g theme_nerd_fonts yes
