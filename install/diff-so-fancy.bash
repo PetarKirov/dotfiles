6cd46b14e96f8c8a47f408c9eb7d3125d94664e0
ZombineDev <petar.p.kirov@gmail.com>
2019-05-28 15:50:16 +0300

    [install/]: Add diff-so-fancy

---
+++ install/diff-so-fancy.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+url="https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy"
+dir="$HOME/.local/bin"
+
+mkdir -p $dir
+curl -fsSL $url > "$dir/diff-so-fancy"
+chmod 755 "$dir/diff-so-fancy"
