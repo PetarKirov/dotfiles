3f0842392b8867774ae1fd96db4058864f770e1d
ZombineDev <petar.p.kirov@gmail.com>
2019-06-12 15:20:53 +0300

    [install/]: Add script for installing NeoVim
    Tested on Debian, Ubuntu, Arch Linux and LinuxBrew (should work on
    macOS/HomeBrew).

---
+++ install/nvim.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+DIR="$(dirname ${BASH_SOURCE[0]})"
+. "$DIR/detect_os.bash"
+
+if [ "$DIST" == 'ubuntu' ] || [ "$DIST" == 'debian' ]; then
+    if [ "$DIST" != 'debian' ]; then
+        # NeoVim is part of Debian, while on Ubuntu we need to add a PPA
+        $SUDO apt-get update
+        $SUDO apt-get install software-properties-common -y
+        $SUDO add-apt-repository ppa:neovim-ppa/stable -y
+    fi
+    $SUDO apt-get update
+    $SUDO apt-get install curl git neovim -y
+elif [ "$DIST" == 'arch' ]; then
+    $SUDO pacman -Syu curl git neovim --noconfirm
+elif [ "$OS" == 'osx' ]; then
+    brew install curl git neovim
+fi
+
+curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh \
+  | bash -s -- "$HOME/.cache/dein"
