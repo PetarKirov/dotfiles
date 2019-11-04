b3418b730961666e0e2a27e7e6c672a66eeb3f57
ZombineDev <petar.p.kirov@gmail.com>
2019-11-04 20:33:25 +0200

    [test/run_all_scripts.bash]: Add support for skipping a list of disabled tests

--- test/run_all_scripts.bash
+++ test/run_all_scripts.bash
@@ -4,6 +4,7 @@ set -euo pipefail
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../" && pwd)"
 IMAGE="$1"
 set -x
+DISABLED_TEST_BUNDLES=(${2:-})
 
 if [ "$IMAGE" != 'NATIVE' ]; then
     docker build --network=host \
@@ -13,6 +14,10 @@ if [ "$IMAGE" != 'NATIVE' ]; then
 fi
 
 for bundle in $DIR/test/bundles/*; do
+    bundle_name=$(basename $bundle '.bash')
+    if [[ " ${DISABLED_TEST_BUNDLES[@]} " =~ " ${bundle_name} " ]]; then
+        continue
+    fi
     bundle_relative_path=$(realpath --relative-to="$DIR" "$bundle")
     if [ "$IMAGE" != 'NATIVE' ]; then
         "$DIR/test/install_on_docker.bash" "my_$IMAGE" "$bundle_relative_path"
