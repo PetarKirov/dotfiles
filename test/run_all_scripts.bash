50efe4587e753b9266cf79c4bdf4117667c0641a
ZombineDev <petar.p.kirov@gmail.com>
2019-11-04 18:37:49 +0200

    [circleci]: Test different OSes in parallel

---
+++ test/run_all_scripts.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../" && pwd)"
+IMAGE="$1"
+set -x
+
+if [ "$IMAGE" != 'NATIVE' ]; then
+    docker build --network=host \
+        -t "my_$IMAGE" \
+        -f "$DIR/test/docker/${IMAGE}.Dockerfile" \
+        "$DIR"
+fi
+
+for bundle in $DIR/test/bundles/*; do
+    bundle_relative_path=$(realpath --relative-to="$DIR" "$bundle")
+    if [ "$IMAGE" != 'NATIVE' ]; then
+        "$DIR/test/install_on_docker.bash" "my_$IMAGE" "$bundle_relative_path"
+    else
+        "$bundle"
+    fi
+done
