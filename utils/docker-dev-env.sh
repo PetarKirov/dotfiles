65a7d2bf323eafe72e4726185ac762eda6078ba5
ZombineDev <petar.p.kirov@gmail.com>
2019-11-14 18:45:02 +0200

    [utils]: Add docker-dev-env.sh scripts for adding dev utils on top of any docker image

---
+++ utils/docker-dev-env.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -eu
+dir=$(CDPATH='' cd -- "$(dirname -- "$0")"/.. && pwd -P)
+# shellcheck source=install/detect_os.sh
+
+IMAGE="$1"
+
+docker build --no-cache -t "zlx/${IMAGE}_dev" -f- "$dir" <<EOF
+FROM $IMAGE
+COPY . /dotfiles
+RUN /dotfiles/install/fish.sh && \
+    /dotfiles/install/nvim.sh && \
+    /dotfiles/install/dotfiles.sh && \
+    /dotfiles/install/diff-so-fancy.sh
+EOF
