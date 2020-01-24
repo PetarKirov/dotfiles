0906655c33eda10957ee0d38a3be88593b3b53f1
Petar Kirov <petar.p.kirov@gmail.com>
2020-01-24 15:54:16 +0200

    [.config/fish/functions/svg-opt.fish]: Use svgcleaner instead of inkscape + svgo

--- .config/fish/functions/svg-opt.fish
+++ .config/fish/functions/svg-opt.fish
@@ -1,4 +1,9 @@
-function svg-opt
-   inkscape --verb=EditSelectAll --verb=AlignHorizontalCenter --verb=AlignVerticalCenter --verb=FitCanvasToSelectionOrDrawing --verb=FileSave --verb=FileQuit $argv
-   npx svgo $argv
+function svg-opt --argument-names input_file
+    set input_file_ext_dir (split-path (realpath $input_file))
+    set output_path $input_file_ext_dir[3]/$input_file_ext_dir[1]-opt.svg
+    echo "Optimizing '$input_file' and saving it as '$output_path'"
+    svgcleaner \
+        --indent 2 \
+        "$input_file" \
+        "$output_path"
 end
