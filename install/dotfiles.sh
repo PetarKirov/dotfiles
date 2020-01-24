5db5cc46f546b022acdb7cd4987de9fc41f5b7ff
Petar Kirov <petar.p.kirov@gmail.com>
2020-01-24 15:21:00 +0200

    [install/dotfiles.sh]: Preserve mode, ownership and timestamps when copying dotfiles

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -4,5 +4,5 @@ set -eu
 
 dir=$(CDPATH='' cd -- "$(dirname -- "$0")"/.. && pwd -P)
 
-cp -v "$dir/.gitconfig" "$HOME"
-cp -Rv "$dir/.config" "$HOME"
+cp -vp "$dir/.gitconfig" "$HOME"
+cp -Rvp "$dir/.config" "$HOME"
