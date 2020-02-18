a273915c3ddffeddff974c9a107eaac17dcff6ab
Petar Kirov <petar.p.kirov@gmail.com>
2020-02-18 17:01:17 +0200

    [fish/functions/git-init]: Fetch .editorconfig from GitHub, instead of using hard-coded content

--- .config/fish/functions/git-init.fish
+++ .config/fish/functions/git-init.fish
@@ -4,20 +4,6 @@ function git-init --argument-names language
       wget -q -O .gitignore https://raw.githubusercontent.com/github/gitignore/master/{$language}.gitignore
     end
     if not test -f .editorconfig
-      echo > .editorconfig "\
-# EditorConfig file: http://EditorConfig.org
-root = true
-
-[*]
-charset = utf-8
-end_of_line = lf
-insert_final_newline = true
-trim_trailing_whitespace = true
-indent_style = space
-indent_size = 4
-max_line_length = 80
-
-[*.{yml,yaml,dart,js,jsx,ts,tsx,Dockerfile}]
-indent_size = 2"
+      wget -q -O .editorconfig https://raw.githubusercontent.com/PetarKirov/dotfiles/master/.editorconfig
     end
 end
