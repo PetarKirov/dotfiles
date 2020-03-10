ed7ec05827f47c2b042fdb57893adc96108cfbd7
Petar Kirov <petar.p.kirov@gmail.com>
2020-03-10 13:17:36 +0200

    [fish/functions/enforce]: Improve func so it can be used for parameter validation

--- .config/fish/functions/enforce.fish
+++ .config/fish/functions/enforce.fish
@@ -1,6 +1,12 @@
-function enforce
-    if not test $argv[1..-2]
-        echo $argv[-1] 1>&2
-        exit 42
+function enforce --argument-names input parameter_name
+    if test -z "$input"
+        if test -z "$parameter_name"
+            echo "Required parameter missing." 1>&2
+        else
+            echo "Required parameter `$parameter_name` missing." 1>&2
+        end
+        return 1
+    else
+        echo "$input"
     end
 end
