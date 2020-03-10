216a66bc29a0b28cd5241f28acb30f153ae1e6a4
Petar Kirov <petar.p.kirov@gmail.com>
2020-03-10 14:20:08 +0200

    [utils/docker-dev-env]: Optimize Dockerfile for layer caching

--- utils/docker-dev-env.sh
+++ utils/docker-dev-env.sh
@@ -7,9 +7,20 @@ IMAGE="$1"
 
 docker build -t "zlx/${IMAGE}_dev" -f- "$dir" <<EOF
 FROM $IMAGE
-COPY . /dotfiles
-RUN /dotfiles/install/dotfiles.sh
+
+# Copy individual scripts as needed to improve docker layer caching
+COPY install/detect_os.sh install/diff-so-fancy.sh /dotfiles/install/
 RUN /dotfiles/install/diff-so-fancy.sh --docker
+
+COPY install/fish.sh /dotfiles/install/
 RUN /dotfiles/install/fish.sh
+
+COPY install/nvim.sh /dotfiles/install/
+COPY .config/nvim/init.vim /dotfiles/.config/nvim/init.vim
 RUN /dotfiles/install/nvim.sh
+
+# Copy the rest of the files in one go, as there's nothing left
+# that could benefit from caching.
+COPY . /dotfiles/
+RUN /dotfiles/install/dotfiles.sh
 EOF
