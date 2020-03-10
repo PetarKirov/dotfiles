e187148cab70129bc68a53230ee3315fe2716110
Petar Kirov <petar.p.kirov@gmail.com>
2020-03-10 13:18:25 +0200

    [fish/functions/svg-opt]: Add `--inline` option to allow opt-in overwriting of files
    Also use argparse for argument parsing and validation.

--- .config/fish/functions/svg-opt.fish
+++ .config/fish/functions/svg-opt.fish
@@ -1,6 +1,13 @@
-function svg-opt --argument-names input_file
-    set input_file_ext_dir (split-path (realpath $input_file))
-    set output_path $input_file_ext_dir[3]/$input_file_ext_dir[1]-opt.svg
+function svg-opt
+    argparse 'i-inline' 'f-input=' -- $argv
+    set -l input_file (enforce "$_flag_input" "--input"); or return
+    set -l inline $_flag_inline
+    if test -z "$inline"
+        set input_file_ext_dir (split-path (realpath $input_file))
+        set output_path $input_file_ext_dir[3]/$input_file_ext_dir[1]-opt.svg
+    else
+        set output_path $input_file
+    end
     echo "Optimizing '$input_file' and saving it as '$output_path'"
     svgcleaner \
         --indent 2 \
