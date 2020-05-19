417be2b3523f941d7b432afc4a8afade2b6b0208
Petar Kirov <petar.p.kirov@gmail.com>
2020-05-19 16:50:48 +0300

    [config.fish]: Set configuration for bobthefish theme:
    theme_use_abbreviated_branch_name -> no
    theme_display_git_master_branch -> yes
    
    Before long branch names were abbreviated, which made them hard to copy-paste.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -58,6 +58,9 @@ set -g theme_newline_cursor yes
 set -g theme_date_format "+%H:%M:%S %F (%a)"
 set -g theme_color_scheme dark
 set -g theme_display_vi yes
+set -g theme_use_abbreviated_branch_name no
+set -g theme_display_git_master_branch yes
+
 # If using fonts from this repo:
 # https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/FiraCode/Regular/complete
 set -g theme_nerd_fonts yes
