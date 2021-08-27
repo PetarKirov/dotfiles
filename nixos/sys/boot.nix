639f7e724b3e41ce401a548437a84afa53bd375a
Petar Kirov <petar.p.kirov@gmail.com>
2021-08-27 20:43:24 +0300

    config(nixos/sys/boot): Allow the Linux kernel package to be overriden per machine

--- nixos/sys/boot.nix
+++ nixos/sys/boot.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  boot.kernelPackages = pkgs.linuxPackages;
+  boot.kernelPackages = pkgs.lib.mkOverride 1 pkgs.linuxPackages;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
