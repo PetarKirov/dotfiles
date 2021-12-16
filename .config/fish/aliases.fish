67b2b911f7bcb7ef819aae48802dc7d6907bedd4
Petar Kirov <petar.p.kirov@gmail.com>
2021-12-16 13:13:17 +0200

    improve(fish/aliases): Allow omitting the feature branch in `grmbr`

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -97,8 +97,8 @@ end
 
 # Safe remove git branch
 function grmbr --argument-names remote master_branch feature_branch
-    test -n "$feature_branch"; or exit 1
-    test -n "$master_branch"; or set master_branch master
+    test -n "$feature_branch"; or set feature_branch (git branch --show-current)
+    test -n "$master_branch"; or begin; echo '`$master_branch` not set'; return 1; end
     test -n "$remote"; or set remote upstream
     if git remote -v | grep -q '^me';
         set origin 'me'
@@ -108,13 +108,17 @@ function grmbr --argument-names remote master_branch feature_branch
         set origin $remote
     end
     echo "Updating '$master_branch' from '$remote' and deleting '$feature_branch' if '$origin/$feature_branch' was deleted"
+    false
     git fetch --multiple --prune $remote $origin
     and git checkout $master_branch
     and git merge --ff-only $remote/$master_branch
-    and git checkout $feature_branch
-    and git rebase $master_branch
-    and git checkout $master_branch
     and git branch -d $feature_branch
+    or begin
+        git checkout $feature_branch
+        git rebase $master_branch
+        git checkout $master_branch
+        git branch -d $feature_branch
+    end
 end
 
 # ----- Direnv: -----
