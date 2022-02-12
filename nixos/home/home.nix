82268dfab114a058444271b6540479af926cb9a6
Petar Kirov <petar.p.kirov@gmail.com>
2022-02-12 17:42:33 +0200

    feat(git,nix): Integrate `delta` git diff/log pager

--- nixos/home/home.nix
+++ nixos/home/home.nix
@@ -132,9 +132,12 @@
   programs.git = {
     enable = true;
     package = pkgs.gitAndTools.gitFull;
+    delta.enable = true;
     includes = [
       { path = ../../.gitconfig; }
       { path = ../../.config/git/aliases.gitconfig; }
+      { path = ../../.config/git/delta.gitconfig; }
+      { path = ../../.config/git/delta-themes.gitconfig; }
     ];
   };
 
