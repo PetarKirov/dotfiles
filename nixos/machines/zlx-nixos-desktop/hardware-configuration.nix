609f00129827bfd103226bab29886badf661557a
Petar Kirov <petar.p.kirov@gmail.com>
2022-02-17 11:55:06 +0200

    refactor(nixos/machines): Inline `nixos/modules/installer/scan/not-detected`
    The import of this module relied on NIXPATH, requiring the `--impure`
    flag to be passed to `nixos-rebuild switch --flake ...`.  In theory, I
    could import this module in a pure way, but considering that it's just a
    single line of configuration, it's more practical to inline it.

--- nixos/machines/zlx-nixos-desktop/hardware-configuration.nix
+++ nixos/machines/zlx-nixos-desktop/hardware-configuration.nix
@@ -4,14 +4,11 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
-    ];
-
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+  hardware.enableRedistributableFirmware = true;
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/477de8f0-6847-4a57-beb0-712beba1e5fb";
