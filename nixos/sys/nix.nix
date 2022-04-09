3d82e5bde10e5bcbc0af02f6973fbb8e0871a819
Petar Kirov <petar.p.kirov@gmail.com>
2022-04-10 00:11:33 +0300

    config(nixos/sys/nix): Trust the default user
    This allows it to set additional substituters (binary caches).

--- nixos/sys/nix.nix
+++ nixos/sys/nix.nix
@@ -1,7 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, defaultUser, ... }:
 {
   nix = {
     package = pkgs.nix_2_7;
+    trustedUsers = [ "root" defaultUser ];
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
