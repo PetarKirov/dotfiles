c195eb8f1b5c87317a2b2058dbf19392c4856fa7
Petar Kirov <petar.p.kirov@gmail.com>
2022-05-14 16:32:37 +0300

    style(install/nvm): Fix indentation

--- install/nvm.sh
+++ install/nvm.sh
@@ -5,24 +5,24 @@ dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
 . "$dir/detect_os.sh"
 
 if [ "$OS" = 'linux' ] || [ "$OS" = 'osx' ]; then
-	if [ "$DIST" = 'ubuntu' ] || [ "$DIST" = 'debian' ]; then
-		$SUDO apt-get update
-		$SUDO apt-get install bash curl -y
-	elif [ "$DIST" = 'arch' ]; then
-		$SUDO pacman -Syu bash curl --noconfirm
-	elif [ "$DIST" = 'alpine' ]; then
-		$SUDO apk add bash curl
-	elif [ "$DIST" = 'homebrew' ]; then
-		brew install bash curl
-	fi
-    url="https://github.com/nvm-sh/nvm/raw/v0.34.0/install.sh"
-    curl -fsSL "$url" | /usr/bin/env bash
+  if [ "$DIST" = 'ubuntu' ] || [ "$DIST" = 'debian' ]; then
+    $SUDO apt-get update
+    $SUDO apt-get install bash curl -y
+  elif [ "$DIST" = 'arch' ]; then
+    $SUDO pacman -Syu bash curl --noconfirm
+  elif [ "$DIST" = 'alpine' ]; then
+    $SUDO apk add bash curl
+  elif [ "$DIST" = 'homebrew' ]; then
+    brew install bash curl
+  fi
+  url="https://github.com/nvm-sh/nvm/raw/v0.34.0/install.sh"
+  curl -fsSL "$url" | /usr/bin/env bash
 
-    if type fish >/dev/null 2>&1 && fish -c 'type omf' >/dev/null 2>&1; then
-        fish -c 'omf install https://github.com/edc/bass'
-        fish -c 'omf install https://github.com/FabioAntunes/fish-nvm'
-    fi
+  if type fish >/dev/null 2>&1 && fish -c 'type omf' >/dev/null 2>&1; then
+    fish -c 'omf install https://github.com/edc/bass'
+    fish -c 'omf install https://github.com/FabioAntunes/fish-nvm'
+  fi
 else
-    echo "Unsupported platform: DIST: '$DIST' OS: '$OS'"
-    return 13
+  echo "Unsupported platform: DIST: '$DIST' OS: '$OS'"
+  return 13
 fi
