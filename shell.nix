88b2ff952854fc5d63b42caecf875c5497a1115a
Petar Kirov <petar.p.kirov@gmail.com>
2022-05-31 14:30:58 +0000

    fix(shell.nix): Don't use  in top-level bash snippet

--- shell.nix
+++ shell.nix
@@ -6,7 +6,7 @@ mkShell {
   ];
 
   shellHook = ''
-    local remote_url="$(git config --get remote.origin.url)"
+    remote_url="$(git config --get remote.origin.url)"
     project_path=$(echo "$remote_url" | sed -E 's"(git@|.+://)[^:/]+[:/](.+)/.+$"\2"')
     repo_name=$(echo "$remote_url" | sed -E -e 's|.+/(.+)$|\1|' -e 's|(.+)(.git)$|\1|')
 
