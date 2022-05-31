77107bf32adbed3fd722dab3cf6cf0e5d3ebf639
Petar Kirov <petar.p.kirov@gmail.com>
2022-05-31 14:13:05 +0000

    config(nixos/machies): Add mcl-nixos-desktop01

---
+++ nixos/machines/mcl-nixos-desktop01/hardware-configuration.nix
@@ -0,0 +1,16 @@
+{lib, ...}: {
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  imports = [./file-systems.nix];
+
+  networking.useDHCP = lib.mkDefault true;
+  networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
+
+  hardware.enableRedistributableFirmware = true;
+  hardware.cpu.intel.updateMicrocode = true;
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
+}
