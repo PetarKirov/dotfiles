1804e65eea7db7d7ef3c1a74b63d0483639d1328
Petar Kirov <petar.p.kirov@gmail.com>
2022-06-07 11:47:12 +0300

    config(utils/commit_flake_update): Allow usage of the script outside CI environment

--- utils/commit_flake_update.bash
+++ utils/commit_flake_update.bash
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
 
-git config --local user.email "out@space.com"
-git config --local user.name "beep boop"
+if ! git config --get user.name >/dev/null 2>&1 || \
+  [ "$(git config --get user.name)" = "" ] ||
+  ! git config --get user.email >/dev/null 2>&1 || \
+  [ "$(git config --get user.email)" = "" ]; then
+  echo "git config user.{name,email} is not set - configuring"
+  set -x
+  git config --local user.email "out@space.com"
+  git config --local user.name "beep boop"
+fi
 
 nix flake update --commit-lock-file
 git log -1 '--pretty=format:%b' | sed '1,2d' > commit_msg_body
