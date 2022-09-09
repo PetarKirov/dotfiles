350394de179356654d806c6a6bf8347d24058bb1
Petar Kirov <petar.p.kirov@gmail.com>
2022-09-09 19:59:22 +0300

    config(machines/zlx-flow-x13): Enable swap

--- nixos/machines/zlx-flow-x13/file-systems.nix
+++ nixos/machines/zlx-flow-x13/file-systems.nix
@@ -28,5 +28,10 @@ in {
       "userdata/home"
     ];
 
-  swapDevices = [];
+  swapDevices = [
+    {
+      device = "/dev/disk/by-partuuid/05b637d9-a94b-4ac0-a1de-2bd51a23366a";
+      randomEncryption = true;
+    }
+  ];
 }
