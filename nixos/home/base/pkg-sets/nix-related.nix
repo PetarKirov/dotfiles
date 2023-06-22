aedb1329b2acc2c825beb6e531090224f62f18f7
Petar Kirov <petar.p.kirov@gmail.com>
2023-06-22 10:20:42 +0300

    add_pkg(home/base): Add `nixd`

--- nixos/home/base/pkg-sets/nix-related.nix
+++ nixos/home/base/pkg-sets/nix-related.nix
@@ -1,15 +1,20 @@
 {
   pkgs,
   unstablePkgs,
+  inputs',
   ...
 }: {
-  home.packages = with pkgs; [
-    cachix
-    unstablePkgs.nurl
-    unstablePkgs.nix-init
-    nix-tree
-    patchelf
-    alejandra
-    nix-output-monitor
-  ];
+  home.packages = with pkgs;
+    [
+      cachix
+      unstablePkgs.nurl
+      unstablePkgs.nix-init
+      nix-tree
+      patchelf
+      alejandra
+      nix-output-monitor
+    ]
+    ++ [
+      inputs'.nixd.packages.default
+    ];
 }
