a1cf4b9d5e7fe1e759d141275b08bf72a13455a7
Petar Kirov <petar.p.kirov@gmail.com>
2023-07-10 18:27:42 +0300

    config(machines/zlx-flow-x13): Enable Asusctl drivers

--- nixos/machines/zlx-flow-x13/configuration.nix
+++ nixos/machines/zlx-flow-x13/configuration.nix
@@ -2,6 +2,11 @@
   networking.hostId = "cc12d73b";
   system.stateVersion = "22.05";
 
+  services.asusd = {
+    enable = true;
+    enableUserService = true;
+  };
+
   services.zfs = {
     trim.enable = true;
     autoScrub.enable = true;
