6f93ff060d5400e5904c4ae490ce61528bbf7c36
Petar Kirov <petar.p.kirov@gmail.com>
2023-07-23 14:36:51 +0300

    feat(nixos/nix): Enable distributed builds

--- nixos/sys/nix.nix
+++ nixos/sys/nix.nix
@@ -5,9 +5,13 @@
 }: {
   nix = {
     package = pkgs.nixVersions.nix_2_16;
+    distributedBuilds = true;
     settings = {
       trusted-users = ["root" defaultUser];
       experimental-features = ["nix-command" "flakes"];
+      max-jobs = "auto";
+      builders-use-substitutes = true;
+      builders = "@/etc/nix/machines";
     };
     generateNixPathFromInputs = true;
     generateRegistryFromInputs = true;
