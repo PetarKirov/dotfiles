06b592f854408ce26ed9ee2c86884a2aa90d6a22
Petar Kirov <petar.p.kirov@gmail.com>
2023-08-23 16:17:24 +0300

    feat(sys/services): Enable home-assistant

--- nixos/sys/packages.nix
+++ nixos/sys/packages.nix
@@ -7,6 +7,19 @@
   services.openssh.enable = true;
   services.yubikey-agent.enable = true;
 
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers.homeassistant = {
+      volumes = ["home-assistant:/config"];
+      environment.TZ = "Europe/Berlin";
+      image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
+      extraOptions = [
+        "--network=host"
+        # "--device=/dev/ttyACM0:/dev/ttyACM0" # Example, change this to match your own hardware
+      ];
+    };
+  };
+
   fonts.fonts = with pkgs; [
     (nerdfonts.override {fonts = ["DroidSansMono" "FiraCode" "FiraMono"];})
   ];
