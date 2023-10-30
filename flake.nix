d702821a06f1505630cc71803cf78d72ea75fa13
Petar Kirov <petar.p.kirov@gmail.com>
2023-10-30 18:55:32 +0200

    config(flake.nix): Update `defaultUser`

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
     flake-parts,
     ...
   } @ inputs: let
-    defaultUser = "zlx";
+    defaultUser = "petar";
   in
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
